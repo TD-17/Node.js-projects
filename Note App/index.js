9c50e726facf116a0a90f15d24d27bcd67b300ec
Tanya Dixit <shanya.2018td@gmail.com>
2020-07-11 15:22:02 +0530

    completed notes.ejs and index.js files

--- Note App/index.js
+++ Note App/index.js
@@ -3,14 +3,40 @@ const http = require('http');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
+let note = [{ id: 1, body: 'This is first text' }, { id: 2, body: 'This is a second text' }];
+
 const hostname = 'localhost';
 const port = 3000;
 
-const app = express();
+let app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({
+    extended: true
+    }));
 app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('notes', {
+      note: note
+    });
+  });
+
+  app.post("/addNotes",(req, res) => {
+    const userNote = {};
+    userNote.id = Math.random() * 100;
+    userNote.body = req.body.newNote
+    note.push(userNote);
+    res.redirect('/');
+  });
+
+  app.post('/deleteNote/:id', (req, res) => {
+    console.log(req.params.id);
+    const deleteNotes = note.filter(item => item.id != req.params.id);
+    note = deleteNotes;
+    return res.redirect('/');
+  });
 
 app.use((req, res, next) => {
     res.statusCode = 200;
